04cadcfacb6b281c800523725e74819ab9c1e806
Yespanic <yespanic@gmail.com>
2019-11-15 16:26:47 +0300

    действия для rest-controller

--- controllers/RestController.php
+++ controllers/RestController.php
@@ -2,9 +2,60 @@
 
 namespace app\controllers;
 
-use yii\rest\ActiveController;
+use app\models\Places;
+use yii\filters\auth\HttpBasicAuth;
+use yii\rest\Controller;
 
-class RestController extends ActiveController
+class RestController extends Controller
 {
-    public $modelClass = 'app\models\Places';
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+
+//        $behaviors['authentificator'] = [
+//            'class' => HttpBasicAuth::className(),
+//        ];
+
+        return $behaviors;
+    }
+
+    public function actions()
+    {
+        return parent::actions();
+    }
+
+//    public function actionGetAll()
+//    {
+//        $places = Places::find()->all();
+//
+//        return $this->asJson($places);
+//    }
+
+    public function actionGetCountry($country)
+    {
+        $places = Places::find()->where(['like', 'country',  $country])->all();
+
+        return $this->asJson($places);
+    }
+
+    public function actionGetRegion($region)
+    {
+        $places = Places::find()->where(['like', 'region',  $region])->all();
+
+        return $this->asJson($places);
+    }
+
+    public function actionGetId($id)
+    {
+        $places = Places::find()->where(['id' => $id])->all();
+
+        return $this->asJson($places);
+    }
+
+    public function actionGetIndex($number)
+    {
+        $places = Places::find()->where(['number' => $number])->all();
+
+        return $this->asJson($places);
+    }
 }
