2d45712233f3be09d66b1fe018af79194baa1295
Yespanic <yespanic@gmail.com>
2020-01-17 12:26:22 +0300

    создание представления и компонентов для переведения слов в числа

---
+++ views/site/about.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\helpers\Html;
+use yii\web\UploadedFile;
+
+/* @var $this yii\web\View */
+
+$this->title = 'Project2';
+?>
+
+<div class="">
+
+    <p><?= Html::a(Html::img('@web/images/first.jpg', ['alt'=>'dick', 'style width'=>'100%']), ['/places']) ?></p>
+
+</div>
+
+
+
+
+
+
